c2ca0b1f99b7ecccf262da11fc00f1b4941d4d08
fetraandri <hei.fetra@gmail.com>
2024-02-02 10:04:15 -0500

    fix: fix facebook authentication integration and firebase

--- src/pages/login/index.tsx
+++ src/pages/login/index.tsx
@@ -5,29 +5,25 @@ import Cookies from 'js-cookie';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import { initializeApp } from "firebase/app";
 import { getAnalytics } from "firebase/analytics";
-import styles from '../../styles/login.module.css'
-
-const firebaseConfig = {
-  apiKey: "AIzaSyDieTZ3ouciT5TcNQzB_GDmaFVl-WIDp_c",
-  authDomain: "noble-maxim-399517.firebaseapp.com",
-  projectId: "noble-maxim-399517",
-  storageBucket: "noble-maxim-399517.appspot.com",
-  messagingSenderId: "744626026334",
-  appId: "1:744626026334:web:cf8d84e550fe14d5df41cf",
-  measurementId: "G-E3JTJ4W88K"
-};
-
-const app = initializeApp(firebaseConfig);
+import styles from '../../styles/login.module.css';
 
 const LoginForm = () => {
   const router = useRouter();
   const [loginError, setLoginError] = useState(false);
 
   useEffect(() => {
-    if (typeof window !== "undefined") {
-      getAnalytics(app);
-    }
-  }, []); // Utilisez un effet pour initialiser Analytics après le rendu initial
+    const firebaseConfig = {
+      apiKey: "AIzaSyDC239x3b3AyxAO1c4iq_zILKDV9aZCqEc",
+      authDomain: "social-73969.firebaseapp.com",
+      projectId: "social-73969",
+      storageBucket: "social-73969.appspot.com",
+      messagingSenderId: "1059736885623",
+      appId: "1:1059736885623:web:86f724a4cbc27a3d032972",
+      measurementId: "G-8PHGXRW4H3"
+    };
+    const app = initializeApp(firebaseConfig);
+    getAnalytics(app);
+  }, []); 
 
   const handleFacebookLogin = async () => {
     try {
@@ -37,18 +33,23 @@ const LoginForm = () => {
       const result = await signInWithPopup(auth, provider);
       const credential = FacebookAuthProvider.credentialFromResult(result);
       const token = credential?.accessToken;
+      console.log("Token généré avec succès :", token); // Afficher le token dans la console
       const idUser = result.user.uid;
       Cookies.set('id', idUser);
       if (token) {
         Cookies.set('token', token);
       }
       router.push('/profile');
-    } catch (error) {
+    } catch (error: any) {
       console.error(error);
-      setLoginError(true);
+      if (error.code === 'auth/cancelled-popup-request' || error.code === 'auth/popup-closed-by-user') {
+        console.log("L'authentification a été annulée par l'utilisateur.");
+      } else {
+        setLoginError(true);
+      }
     }
   };
-  
+
   return (
     <div className="container-fluid">
       <div className="row">
@@ -66,11 +67,11 @@ const LoginForm = () => {
         </div>
         <div className="col-md-6">
           <div className={`${styles.rightPanel} d-flex align-items-center justify-content-center`}>
-            <div className={`${styles.imageWrapper} w-100`}> {/* Ajout de la classe w-100 pour occuper toute la largeur */}
+            <div className={`${styles.imageWrapper} w-100`}>
               <img
                 loading="lazy"
                 src="https://cdn.builder.io/api/v1/image/assets/TEMP/3aeed1fa4306d23c2ef6a1e0ee18b85766ffd2e7417569bf24150be1770bf50d?apiKey=1c9e99217dbe4efba418406c62e45be9&"
-                className={`${styles.image} img-fluid`} // Ajout de la classe img-fluid pour rendre l'image responsive
+                className={`${styles.image} img-fluid`}
                 alt="Image"
               />
             </div>
@@ -89,9 +90,6 @@ const LoginForm = () => {
       </div>
     </div>
   );
-  
-  
-  
 };
 
 export default LoginForm;
