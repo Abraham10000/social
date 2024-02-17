ac29165d245b7e315c5be0f272b946302e898a4b
Abraham10000 <hei.abraham.4@gmail.com>
2024-02-17 12:09:08 +0300

    fix : add advice section to landing page

--- src/pages/landing/index.tsx
+++ src/pages/landing/index.tsx
@@ -8,13 +8,13 @@ const LandingPage = (iconApp : string) => {
     <div className={`${styles.bodyContainer}`}>    
         <div className="container-fluid py-3" id={`${styles.component}`}>
             <div className="bg-transparent">
-                <NavBar title="SENTINENT" iconSrc="/lg.png"/>
+                <NavBar title="SENTINENT" iconSrc="/lg.png"/> 
                 <div className={`${styles.wrapper}`}>
                     <div className="col-xxl-11">
                         <div style={{display : "flex", flexDirection : "column", gap : "24px", alignItems : "center"}}>
                             <div style={{display : "flex", flexDirection : "column", gap : "24px", alignItems : "center"}}>
-                                <p style={{fontSize : "4rem", fontWeight  : "bold"}}
-                                className="text-center text-dark font-family-Montserrat col-xxl-4 m-0 px-3 py-2">
+                                <p style={{fontSize : "3.5rem", fontWeight  : "bold"}}
+                                className="text-center text-dark font-family-Montserrat col-xxl-12 m-0 px-3 py-2">
                                     Bienvenue sur Sentiment !
                                 </p>
                                 <p className="text-center text-dark fs-6 fs-4 fw-normal font-family-Montserrat col-xxl-6 m-0 px-3 py-2">
@@ -42,6 +42,38 @@ const LandingPage = (iconApp : string) => {
                             <button className={`${styles.connectezVous}`}>
                                 <p style={{margin : "18px", color : "antiquewhite"}}>Connectez-vous avec Facebook</p>
                             </button>
+                        </div>
+                    </div>
+                    <p className="text-center text-dark fs-6 fw-normal font-family-Montserrat col-xxl-6 m-0 px-3 py-2">
+                        Voici un exemple de proposition réalisée en quelques clics 👇
+                    </p>
+                    <p id="about" className="text-primary fs-5 fw-bold font-family-Montserrat col-xxl-2 m-0 px-3 py-2">
+                        Comment ça marche ?
+                    </p>
+                    <div className="px-1 py-1 col-xxl-12 row justify-content-center align-items-center">
+                        <div className={`${styles.step}`}>
+                            <Image alt="detail" src="/detail1.png" width={569.33} height={337.38} style={{borderRadius : "25px"}}/>
+                            <div className={`${styles.advice}`}>
+                                <div className={`${styles.number}`}>
+                                    <div className={`${styles.un}`}>01.</div>
+                                </div>
+                                <b><div className={`${styles.textAdvice}`}>
+                                    Connectez-vous à partir de votre compte Facebook 
+                                </div></b>
+                            </div>
+                        </div>
+                    </div>
+                    <div className="px-1 py-1 col-xxl-12 row justify-content-center align-items-center">
+                        <div className={`${styles.step}`}>
+                            <div className={`${styles.advice}`}>
+                                <div className={`${styles.number}`}>
+                                    <div className={`${styles.un}`}>02.</div>
+                                </div>
+                                <b><div className={`${styles.textAdvice}`}>
+                                    Et recevez vos résultats sous forme d’un statistique 
+                                </div></b>
+                            </div>
+                            <Image alt="detail" src="/detail2.png" width={569.33} height={337.38} style={{borderRadius : "25px"}}/>
                         </div>
                     </div>
                 </div>
