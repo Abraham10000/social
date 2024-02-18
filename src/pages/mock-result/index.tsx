e1dcbf0edb8cd6cf22daa75879ff903cdf69c42a
Abraham10000 <hei.abraham.4@gmail.com>
2024-02-18 16:19:02 +0300

    fix : add result page

--- src/pages/mock-result/index.tsx
+++ src/pages/mock-result/index.tsx
@@ -1,14 +1,11 @@
-import styles from '../../styles/dashboard.module.css';
+import styles from '../../styles/result.module.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import React, { useEffect, useState } from "react";
 import { useProfile } from "../../utils/ProfileContext";
 import Chart from "chart.js/auto";
 import Cookies from "js-cookie";
-import Link from 'next/link';
 
-
-export default function Dashboard() {
-
+export default function Result() {
     return (
         <div className={`${styles.bodyContainer} bodyContainer`} style={{display: "flex", flexDirection: "column", alignItems: "flex-start"}}>
             <div id="component" className="container-fluid py-2">
@@ -52,35 +49,26 @@ export default function Dashboard() {
                     <p className="m-0">Bienvenu sur My App</p>
                     <p className="m-0">Votre nom</p>
                 </div>
-                <div style={{width: "100%"}}>
-                    <div className={`${styles.anotherStyle} anotherStyle`}>
-                        <p style={{fontWeight: "700"}}>Taux de réactions sur facebook</p>
-                        <div style={{display: "flex", justifyContent: "space-between", width: "100%"}}>
-                            <div style={{display: "flex", flexDirection: "column", gap: "12px"}}>
-                                <ul id={`${styles.legend} legend`}>
-                                    <li><span style={{backgroundColor: "#4267B2"}}></span> Like</li>
-                                    <li><span style={{backgroundColor: "#FD7D84"}}></span> Love</li>
-                                    <li><span style={{backgroundColor: "#1AA1F2"}}></span> Wow</li>
-                                    <li><span style={{backgroundColor: "#FAB81E"}}></span> Haha</li>
-                                    <li><span style={{backgroundColor: "#FFAC53"}}></span> Angry</li>
-                                </ul>
-                                <div className={`${styles.analyse} analyse`}>
-                                    <p>Selon l’analyse de votre profil facebook, vous êtes une personnes sensible, Morbi in nibh aliquam consequat sed at et phasellus eros. Eget in dolor mattis </p>
-                                    <Link href="/mock-result"><div style={{borderRadius: "44px"}} className="bg-primary border border-1 border-white border-opacity-50 col-xxl-3 row justify-content-center align-items-center">
-                                        <a href="result.html" style={{textDecoration: "none", width: "fit-content"}} className="text-center text-white p-2 fs-6 fw-normal font-family-Gotham m-0">
-                                            Voir plus de résultat
-                                        </a>
-                                    </div></Link>
-                                </div>
-                            </div>
-                        <div className={`${styles.chartContainer} chartContainer`}>
-                            <canvas id="myChart"></canvas>
-                        </div>
+                <div style={{width: "100%", display: "flex", flexDirection: "column", gap: "24px"}}>
+                    <div className={`${styles.analyse} analyse`}>
+                        <p>
+                            <span className="fw-bold">Feedback</span> :  <br/>
+                        </p>
+                        <p>Selon l’analyse de votre profil facebook, vous êtes une personnes sensible, Morbi
+                            in nibh aliquam consequat sed at et phasellus eros. Eget in dolor mattis </p>
+                    </div>
+                    <div className={`${styles.second}`}>
+                        <p>
+                        <p>
+                            <span className="fw-bold">Résultat</span> : <br/>
+                        </p>
+                        <p>Selon l’analyse de votre profil facebook, vous êtes une personnes sensible, Morbi
+                            in nibh aliquam consequat sed at et phasellus eros. Eget in dolor mattis </p>
+                        </p>
                     </div>
                 </div>
             </div>
-            </div>
-            </div>
         </div>
+    </div>
     )
 }
